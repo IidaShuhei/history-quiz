b0fcc3d553e78a9520958880b77addc4dde83bdf
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-11-06 11:37:45 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,14 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+ALLOWED_HOSTS = ["history-quiz.herokuapp.com"]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -112,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEBUG = False
+
+try:
+    from config.local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
